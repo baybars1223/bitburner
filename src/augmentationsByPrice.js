ce70d79228275d32a03b76c2a7973ca32dd93e81
Admin M <mjthomasson@gmail.com>
2025-08-25 19:24:17 -0600

    add rep filter to augs listing also a sloppy edit for companies

--- src/augmentationsByPrice.js
+++ src/augmentationsByPrice.js
@@ -1,10 +1,14 @@
+import { NS } from "@ns";
+
 /** @param {NS} ns */
 export async function main(ns) {
+	const [repFilter = true] = ns.args
+	const omitFactions = ns.args.slice(1)
 	const factions = Object.values(ns.enums.FactionName)
-	const filtered = factions.filter((faction) => ns.singularity.getFactionRep(faction) > 0)
+	const currentFactions = factions.filter((faction) => ns.singularity.getFactionRep(faction) > 0 && omitFactions.indexOf(faction) === -1)
 
 	const augmentations = {}
-	for (let faction of filtered) {
+	for (let faction of currentFactions) {
 		let offered = ns.singularity.getAugmentationsFromFaction(faction)
 		for (let aug of offered) {
 			if(!augmentations[aug]) {
@@ -27,19 +31,23 @@ export async function main(ns) {
 	
 	for (let aug of Object.keys(augmentations)) {
 		augmentations[aug].price = ns.singularity.getAugmentationPrice(aug)
+		augmentations[aug].rep = ns.singularity.getAugmentationRepReq(aug)
 	}
 
-	let output = Object.entries(augmentations).sort(([aName, {price: aPrice}], [bName, {price: bPrice}]) =>
+	let sortedByPrice = Object.entries(augmentations).sort(([aName, {price: aPrice}], [bName, {price: bPrice}]) =>
 	{
 		// ns.tprint(aPrice - bPrice)
 		return aPrice - bPrice
 	})
-	
 	// TODO: need to check for rep
 	let money = ns.getPlayer().money
-	for(let [name, {factions, price}] of output) {
-		if( price <= money) {
-			ns.tprint(`${name}: ${ns.formatNumber(price,2)} - [${factions.join(', ')}]`)
+	let output = '\n'
+	for(let [name, {factions, price, rep}] of sortedByPrice) {
+		let enoughRep = repFilter ? factions.reduce((acc, cur) => {return acc || ns.singularity.getFactionRep(cur) >= rep}, false) : true
+		if(price <= money && enoughRep) {
+			let n = (name.slice(0,15) + ':')
+			output+=`${n.padEnd(16,' ')} ${ns.formatNumber(price,2).padStart(7)} | ${ns.formatNumber(rep,1).padStart(7)} - [${factions.join(', ')}]\n`
 		}
 	}
+	ns.tprint(output)
 }
